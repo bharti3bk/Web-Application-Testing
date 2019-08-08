4dc5a9c1f44a4e9f641ef01e6c990c984b14841a
Bharti <bharti@Admins-MacBook-Pro.local>
2019-08-08 13:36:28 -0700

    Added buttons to thePage

--- component-testing/src/components/Dashboard.js
+++ component-testing/src/components/Dashboard.js
@@ -3,5 +3,30 @@ import Display from "./Display";
 
 export default function Dashboard(){
     const[ball ,setballs] = useState(0);
-    const[strike ,setStrike] =useState(0);
+    const[strike ,setStrike] =useState(0); 
+    
+    const ballsIncrese = () => {
+       if(ball < 4){
+           setballs(0)
+       }
+    } 
+
+    const strikeIncrease = () => {
+      if(strike < 3){
+          setStrike(0)
+      }
+    } 
+
+    const foulIncrease = () => {
+      
+    }
+
+    return(
+        <div>
+            <button onClick={() => strikeIncrease()}>Strike</button>
+            <button onclick = {() => ballsIncrese()}>Ball</button> 
+            <button onClick = {() => foulIncrease()}>Foul</button>
+            <Display ball = {ball} strike = {strike}/>
+        </div>
+    )
 }
